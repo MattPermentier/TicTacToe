e3d51888561cca47f138edbca805dc5e296b9d56
mattp <matt.permentier@gmail.com>
2022-09-21 17:53:50 +0200

    shorter way to test for vertical winners

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const playingField = document.getElementById('playingField');
 let playingSquares = document.querySelectorAll('.playingSquare');
+let playerTurn = document.getElementById('playerTurn');
 const restartBtn = document.getElementById('restart');
 
 playingField.addEventListener('click', clickedSquare);
@@ -13,58 +14,97 @@ let gameBoard = {
   board: [],
 };
 
+// function to get code cleaner and checks where x/o is placed in gameBoard
+function checkWinner(num, xOrY) {
+  return gameBoard.board[num].classList.contains(xOrY);
+}
+
 // gameRules object to check when player wins
 let gameRules = {
   bestPlayer: function () {
-    if (
-      (checkWinner(0, 'x') && checkWinner(1, 'x') && checkWinner(2, 'x')) ||
-      (checkWinner(0, 'o') && checkWinner(1, 'o') && checkWinner(2, 'o'))
-    ) {
-      console.log('We have a winner!');
-      return true;
-    } else if (
-      (checkWinner(3, 'x') && checkWinner(4, 'x') && checkWinner(5, 'x')) ||
-      (checkWinner(3, 'o') && checkWinner(4, 'o') && checkWinner(5, 'o'))
-    ) {
-      console.log('We have a winner!');
-      return true;
-    } else if (
-      (checkWinner(6, 'x') && checkWinner(7, 'x') && checkWinner(8, 'x')) ||
-      (checkWinner(6, 'o') && checkWinner(7, 'o') && checkWinner(8, 'o'))
-    ) {
-      console.log('We have a winner!');
-      return true;
-    } else if (
-      (checkWinner(0, 'x') && checkWinner(3, 'x') && checkWinner(6, 'x')) ||
-      (checkWinner(0, 'o') && checkWinner(3, 'o') && checkWinner(6, 'o'))
-    ) {
-      console.log('We have a winner!');
-      return true;
-    } else if (
-      (checkWinner(1, 'x') && checkWinner(4, 'x') && checkWinner(7, 'x')) ||
-      (checkWinner(1, 'o') && checkWinner(4, 'o') && checkWinner(7, 'o'))
-    ) {
-      console.log('We have a winner!');
-      return true;
-    } else if (
-      (checkWinner(2, 'x') && checkWinner(5, 'x') && checkWinner(8, 'x')) ||
-      (checkWinner(2, 'o') && checkWinner(5, 'o') && checkWinner(8, 'o'))
-    ) {
-      console.log('We have a winner!');
-      return true;
-    } else if (
-      (checkWinner(0, 'x') && checkWinner(4, 'x') && checkWinner(8, 'x')) ||
-      (checkWinner(0, 'o') && checkWinner(4, 'o') && checkWinner(8, 'o'))
-    ) {
-      console.log('We have a winner!');
-      return true;
-    } else if (
-      (checkWinner(2, 'x') && checkWinner(4, 'x') && checkWinner(6, 'x')) ||
-      (checkWinner(2, 'o') && checkWinner(4, 'o') && checkWinner(6, 'o'))
-    ) {
-      console.log('We have a winner');
-      return true;
+    let typesZero = document.querySelectorAll(`[data-type="0"]`);
+    let typesOne = document.querySelectorAll(`[data-type="1"]`);
+    let typesTwo = document.querySelectorAll(`[data-type="2"]`);
+
+    // typesZero = Array.from(typesZero); // type
+    // let check = typesZero.every((item) => { // type
+    //   return item.classList.contains('o'); // x or y
+    // });
+
+    // if (check === true) {
+    //   playerTurn.innerHTML = 'O wins';
+    // }
+
+    function testWinner(type, xO) {
+      type = Array.from(type); // type
+      let check = type.every((item) => {
+        // type
+        return item.classList.contains(xO); // x or y
+      });
+
+      if (check === true) {
+        let playerWin = xO.toUpperCase(0);
+        playerTurn.innerHTML = `${playerWin} wins`;
+      }
     }
+    testWinner(typesZero, 'x');
+    testWinner(typesOne, 'x');
+    testWinner(typesTwo, 'x');
+
+    testWinner(typesZero, 'o');
+    testWinner(typesOne, 'o');
+    testWinner(typesTwo, 'o');
+
+    // if (
+    //   (checkWinner(0, 'x') && checkWinner(1, 'x') && checkWinner(2, 'x')) ||
+    //   (checkWinner(0, 'o') && checkWinner(1, 'o') && checkWinner(2, 'o'))
+    // ) {
+    //   console.log('We have a winner!');
+    //   console.log(gameBoard);
+    //   return true;
+    // } else if (
+    //   (checkWinner(3, 'x') && checkWinner(4, 'x') && checkWinner(5, 'x')) ||
+    //   (checkWinner(3, 'o') && checkWinner(4, 'o') && checkWinner(5, 'o'))
+    // ) {
+    //   console.log('We have a winner!');
+    //   return true;
+    // } else if (
+    //   (checkWinner(6, 'x') && checkWinner(7, 'x') && checkWinner(8, 'x')) ||
+    //   (checkWinner(6, 'o') && checkWinner(7, 'o') && checkWinner(8, 'o'))
+    // ) {
+    //   console.log('We have a winner!');
+    //   return true;
+    // } else if (
+    //   (checkWinner(0, 'x') && checkWinner(3, 'x') && checkWinner(6, 'x')) ||
+    //   (checkWinner(0, 'o') && checkWinner(3, 'o') && checkWinner(6, 'o'))
+    // ) {
+    //   console.log('We have a winner!');
+    //   return true;
+    // } else if (
+    //   (checkWinner(1, 'x') && checkWinner(4, 'x') && checkWinner(7, 'x')) ||
+    //   (checkWinner(1, 'o') && checkWinner(4, 'o') && checkWinner(7, 'o'))
+    // ) {
+    //   console.log('We have a winner!');
+    //   return true;
+    // } else if (
+    //   (checkWinner(2, 'x') && checkWinner(5, 'x') && checkWinner(8, 'x')) ||
+    //   (checkWinner(2, 'o') && checkWinner(5, 'o') && checkWinner(8, 'o'))
+    // ) {
+    //   console.log('We have a winner!');
+    //   return true;
+    // } else if (
+    //   (checkWinner(0, 'x') && checkWinner(4, 'x') && checkWinner(8, 'x')) ||
+    //   (checkWinner(0, 'o') && checkWinner(4, 'o') && checkWinner(8, 'o'))
+    // ) {
+    //   console.log('We have a winner!');
+    //   return true;
+    // } else if (
+    //   (checkWinner(2, 'x') && checkWinner(4, 'x') && checkWinner(6, 'x')) ||
+    //   (checkWinner(2, 'o') && checkWinner(4, 'o') && checkWinner(6, 'o'))
+    // ) {
+    //   console.log('We have a winner');
+    //   return true;
+    // }
   },
 
   tieGame: function () {
@@ -80,14 +120,10 @@ for (const playingSquare of playingSquares) {
   gameBoard.board.push(playingSquare);
 }
 
-// function to get code cleaner and checks where x/o is placed in gameBoard
-function checkWinner(num, xOrY) {
-  return gameBoard.board[num].classList.contains(xOrY);
-}
-
 // check if square is already filled and make a new x/o
 function clickedSquare(e) {
   target = e.target;
+
   if (target.classList.contains('taken')) {
     alert('This square is already taken');
     return;
@@ -102,13 +138,19 @@ const playerType = (num) => {
   target.classList.add('taken');
 
   return num % 2 === 0
-    ? ((target.innerHTML = 'x'), target.classList.add('x'))
-    : ((target.innerHTML = 'o'), target.classList.add('o'));
+    ? ((target.innerHTML = 'x'),
+      target.classList.add('x'),
+      (playerTurn.innerHTML = "Player O's turn"))
+    : ((target.innerHTML = 'o'),
+      target.classList.add('o'),
+      (playerTurn.innerHTML = "Player X's turn"));
 };
 
 function restartGame() {
   for (const playingSquare of playingSquares) {
     playingSquare.innerHTML = '';
     playingSquare.classList.remove('taken', 'x', 'o');
+    playerTurn.innerHTML = "Player O's turn";
+    num = 0;
   }
 }
